6de927a004a75439b78fe6103b31dbead25c9a77
Steven Beshensky <s.beshensky@gmail.com>
2019-07-30 13:06:39 -0500

    feat: add support for complex error handling

--- src/main/kotlin/TestMicroService/Main.kt
+++ src/main/kotlin/TestMicroService/Main.kt
@@ -9,6 +9,27 @@ fun main(args: Array<String>) {
     path("/api/v2") {
         some()
         other()
+
+        exception(IllegalAccessException::class.java) { exception, request, response ->
+            println("Printing Exception Message --- ${exception.message}")
+
+            response.status(403)
+            response.body(exception.message)
+        }
+
+        internalServerError { request, response ->
+            val message = "Internal server error at: ${request.url()}"
+            println(message)
+            response.status(500)
+            message
+        }
+
+        notFound { request, response ->
+            val message = "Request url not found: ${request.url()}"
+            println(message)
+
+            message
+        }
     }
 }
 
--- src/main/kotlin/TestMicroService/Routes/Other/other.kt
+++ src/main/kotlin/TestMicroService/Routes/Other/other.kt
@@ -2,7 +2,6 @@ package TestMicroService.Routes.Other
 import spark.Spark.*;
 
 fun setup() {
-
     path("/sessions") {
         before("") { request, response ->
             println("catch the request before the processing is done")
@@ -13,6 +12,12 @@ fun setup() {
             val test = "new session here"
 
             test
+        }
+        post("") { request, response ->
+            throw IllegalAccessException("403 not allowed from this ip: ${request.ip()}")
+        }
+        put("") {request, response ->
+            throw Exception("General Error")
         }
         after("") { request, response ->
             println("catch the request after the processing is done")
