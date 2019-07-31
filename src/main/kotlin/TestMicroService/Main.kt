1941ceab91714cdde1b985084c72ba6da08351e6
Steven Beshensky <s.beshensky@gmail.com>
2019-07-31 10:38:53 -0500

    feat: move error handlers into a file and use logger inside

--- src/main/kotlin/TestMicroService/Main.kt
+++ src/main/kotlin/TestMicroService/Main.kt
@@ -1,6 +1,7 @@
 package TestMicroService
 import TestMicroService.Routes.Some.setup as some
 import TestMicroService.Routes.Other.setup as other
+import TestMicroService.Routes.ErrorHandlers.setup as errorHandlers
 import spark.Spark.*
 import org.apache.log4j.LogManager
 import org.apache.log4j.PropertyConfigurator
@@ -21,27 +22,7 @@ object App {
         path("/api/v2") {
             some()
             other()
-
-            exception(IllegalAccessException::class.java) { exception, request, response ->
-                println("Printing Exception Message --- ${exception.message}")
-
-                response.status(403)
-                response.body(exception.message)
-            }
-
-            internalServerError { request, response ->
-                val message = "Internal server error at: ${request.url()}"
-                println(message)
-                response.status(500)
-                message
-            }
-
-            notFound { request, response ->
-                val message = "Request url not found: ${request.url()}"
-                println(message)
-
-                message
-            }
+            errorHandlers()
         }
 
         awaitInitialization()
---
+++ src/main/kotlin/TestMicroService/Routes/ErrorHandlers/ErrorHandlers.kt
@@ -0,0 +1,26 @@
+package TestMicroService.Routes.ErrorHandlers
+import TestMicroService.App
+import org.apache.log4j.LogManager
+import spark.Spark.*;
+private val logger = LogManager.getLogger(App::class.java)
+
+fun setup() {
+    exception(IllegalAccessException::class.java) { exception, request, response ->
+        logger.error(exception.message)
+        response.status(403)
+        response.body(exception.message)
+    }
+
+    internalServerError { request, response ->
+        val message = "Internal server error at: ${request.url()}"
+        logger.error(message)
+        response.status(500)
+        message
+    }
+
+    notFound { request, response ->
+        val message = "Request url not found: ${request.url()}"
+        logger.info(message)
+        message
+    }
+}
